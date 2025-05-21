3eddb5ab748077285377381bc64e8fbef34db972
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2025-05-21 04:24:04 +0100

    Refactors configs

---
+++ fake_profile_detector/configs/general.py
@@ -0,0 +1,19 @@
+import os
+
+TEST_SIZE = 0.2
+VAL_SIZE = 0.3
+RANDOM_SEED = 1337
+BASE_DIR = base_dir = os.path.join(
+    os.environ["HOME"],
+    "master",
+    "src",
+    "fake_profile_detector",
+    "datasets",
+)
+SAVE_DIR = base_dir = os.path.join(
+    os.environ["HOME"],
+    "master",
+    "src",
+    "fake_profile_detector",
+    "results",
+)
